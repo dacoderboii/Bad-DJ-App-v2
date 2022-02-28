bb363c21e5dc374257b187e25ad4a76ffbfbc505
dacoderboii <85032650+dacoderboii@users.noreply.github.com>
2022-02-27 21:22:53 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+music1 = "";
+music2 = "";
+
+function preload() {
+    music1 = loadSound("5 - Pigstep (Stereo Mix).mp3");
+    music2 = loadSound("Where Are We Now.MP3");
+}
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
